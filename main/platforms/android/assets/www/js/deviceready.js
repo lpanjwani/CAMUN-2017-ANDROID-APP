517b687f722ebf0c90bf812ed39cc67e38982101
Lavesh Panjwani <lpanjwani@outlook.com>
2016-10-18 16:40:03 +0400

    Handler

--- main/platforms/android/assets/www/js/deviceready.js
+++ main/platforms/android/assets/www/js/deviceready.js
@@ -1,7 +1,7 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-console.log(cordova.file);
+    console.log(cordova.file);
     hockeyapp.start(null, null, "521c947791184a6195eedac70af73cb4");
     hockeyapp.checkForUpdate();
     hockeyapp.addMetaData(null, null { someCustomProp: 23, anotherProp: "Value" });
